6b6f5db8ec61025094737aee4faf37ff9c28a2d3
heimedina <heidimedina23@gmail.com>
2025-05-01 12:43:17 +0200

    form validation

--- src/main/java/com/ideas/springboot/form/controllers/FormController.java
+++ src/main/java/com/ideas/springboot/form/controllers/FormController.java
@@ -2,30 +2,43 @@ package com.ideas.springboot.form.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import com.ideas.springboot.form.domain.Usuario;
+
+import jakarta.validation.Valid;
 
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
-	
+
 	@GetMapping("/form")
-	public String form (Model model) {
+	public String form(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Juan");
+		usuario.setApellido("Medina");
+		usuario.setIdentificador("123456-k");
 		model.addAttribute("titulo", "Formulario usuarios");
+		model.addAttribute("usuario", usuario);
 		return "form";
 	}
-	
-	@PostMapping("/form") 
-	public String procesar(Model model, @RequestParam(name="username") String username,
-			@RequestParam String password,
-			@RequestParam String email) {
+
+	@PostMapping("/form")
+	public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status) {
+		
 		model.addAttribute("titulo", "Resultado form");
-		model.addAttribute("username", username);
-		model.addAttribute("password", password);
-		model.addAttribute("email", email);
+		
+		if(result.hasErrors()) {
+			return "form";
+		}
+		
+		model.addAttribute("usuario", usuario);
+		status.setComplete();
 		return "resultado";
 	}
-	
-	
 
 }
